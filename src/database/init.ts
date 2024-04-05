c8eaa2b37070218c8ea247f8dddfb2c2e7a52bf9
John <johnadibe123@gmail.com>
2024-04-05 11:36:30 +0100

    Add user schema

--- src/database/init.ts
+++ src/database/init.ts
@@ -1,9 +1,11 @@
 import Db from "./index"
+import userModel from "../models/user-model"
 
 const DbInitialize = async () => {
     try {
         // if you want to connect to the database
         await Db.authenticate()
+        userModel.sync({alter: false})
     } catch(error) {
         console.log("Unable to connect to our database", error)
     }
---
+++ src/models/user-model.ts
@@ -0,0 +1,66 @@
+import Db from "../database/index"
+import { IUserModel } from "../interfaces/user-interface"
+import { DataTypes } from "sequelize"
+import {v4 as uuidv4} from "uuid"
+
+const userModel = Db.define<IUserModel>(
+    "userModel", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: () => uuidv4(), // This will be on insertion
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        isEmailVerified: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        accountStatus: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+            allowNull: false,
+        },
+    }, {
+        timestamps: true,
+        tableName:"users",
+        createdAt:"createdAt",
+        updatedAt:"updatedAt"
+    }
+);
+
+export default userModel;
