51b8b2eef573186c22747f780d7af649b98c5476
John <johnadibe123@gmail.com>
2024-04-15 11:38:08 +0100

    Complete registration and login functionality

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,86 +1,160 @@
-import bcrypt from "bcryptjs"
-import {Request, Response} from "express";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import { Request, Response } from "express";
 import UserService from "../services/user-service";
-import { AccountStatus, EmailStatus, UserRoles } from "../interfaces/enum/user-enum";
+import {
+  AccountStatus,
+  EmailStatus,
+  UserRoles,
+} from "../interfaces/enum/user-enum";
 import { IUserCreationBody } from "../interfaces/user-interface";
 import Utility from "../utils/index.utils";
 import { ResponseCode } from "../interfaces/enum/code-enum";
 
 class UserController {
+  private userService: UserService;
 
-    private userService:UserService
+  constructor(_userService: UserService) {
+    // It will take one parameter
+    this.userService = _userService;
+  }
 
-    constructor(_userService:UserService){ // It will take one parameter
-        this.userService = _userService;
+  // structure the data
+  // hash the password
+  // create user
+  // Generate a token for verification
+  // sending the welcom/verification email
+  async register(req: Request, res: Response) {
+    try {
+      const params = { ...req.body };
+      // structure the data
+      const newUser = {
+        firstname: params.firstname,
+        lastname: params.lastname,
+        email: params.email,
+        username: params.email.split("@")[0],
+        password: params.password,
+        role: UserRoles.CUSTOMER,
+        isEmailVerified: EmailStatus.NOT_VERIFIED,
+        accountStatus: AccountStatus.ACTIVE,
+      } as IUserCreationBody;
+
+      // Hash password
+      newUser.password = bcrypt.hashSync(newUser.password, 10);
+
+      // Validate or check if the email exist because we are gettting the username from the email
+      // We will try to fetch the user from the database with that email
+      let userExists = await this.userService.getUserByField({
+        email: newUser.email,
+      });
+
+      if (userExists) {
+        return Utility.handleError(
+          res,
+          "Email already exists",
+          ResponseCode.ALREADY_EXIST
+        );
+      }
+
+      // Create User
+      let user = await this.userService.createUser(newUser);
+      // I dont want it to send a password that was hashed, so we do
+      user.password = "";
+
+      return Utility.handleSuccess(
+        res,
+        "User Registered successfully",
+        { user },
+        ResponseCode.SUCCESS
+      );
+
+      // res.send({message: "Registration successful"})
+    } catch (error) {
+      return Utility.handleError(
+        res,
+        (error as TypeError).message,
+        ResponseCode.SERVER_ERROR
+      );
     }
+  }
 
-    // structure the data
-    // hash the password
-    // create user
-    // Generate a token for verification
-    // sending the welcom/verification email
-    async register(req : Request, res : Response) {
-        try {
-            const params = {...req.body}
-            // structure the data
-            const newUser = {
-                firstname:params.firstname,
-                lastname:params.lastname,
-                email:params.email,
-                username:params.email.split("@")[0],
-                password:params.password,
-                role:UserRoles.CUSTOMER,
-                isEmailVerified:EmailStatus.NOT_VERIFIED,
-                accountStatus:AccountStatus.ACTIVE
-            } as IUserCreationBody;
+  async login(req: Request, res: Response) {
+    try {
+      // Get the params from the request body
+      const params = { ...req.body };
 
-            // Hash password
-            newUser.password = bcrypt.hashSync(newUser.password, 10)
+      // get the user record
+      const user = await this.userService.getUserByField({
+        email: params.email,
+      });
+      if (!user) {
+        return Utility.handleError(
+          res,
+          "Invalid Login details",
+          ResponseCode.NOT_FOUND
+        );
+      }
 
-            // Validate or check if the email exist because we are gettting the username from the email
-            // We will try to fetch the user from the database with that email
-            let userExists = await this.userService.getUserByField({email: newUser.email})
+      // If we found the user then we should compare if the password match
+      let isPasswordMatched = await bcrypt.compare(
+        params.password,
+        user.password
+      );
+      if (!isPasswordMatched) {
+        return Utility.handleError(
+          res,
+          "Invalid Login details",
+          ResponseCode.NOT_FOUND
+        );
+      }
 
-            if(userExists) {
-                return Utility.handleError(res, "Email already exists", ResponseCode.ALREADY_EXIST)
-            }
-
-            // Create User
-            let user = this.userService.createUser(newUser)
-            // I dont want it to send a password that was hashed, so we do
-            user.password = ""
-
-            return Utility.handleSuccess(res, "User Registered successfully", {user}, ResponseCode.SUCCESS)
-
-            // res.send({message: "Registration successful"})
-        } catch (error) {
-            res.send({message: "Server Error"})
+      // So if it matches then we will create a token
+      const token = JWT.sign(
+        {
+          firstname: user.firstname,
+          lastname: user.lastname,
+          id: user.id,
+          email: user.email,
+          role: user.role,
+        },
+        process.env.JWT_KEY as string,
+        {
+          expiresIn: "30d",
         }
-    }
+      );
 
-    async login(req : Request, res : Response) {
-        try {
-            res.send({message: "Login successful"})
-        } catch (error) {
-            res.send({message: "Server Error"})
-        }
+      return Utility.handleSuccess(
+        res,
+        "Login Successful",
+        { user, token },
+        ResponseCode.SUCCESS
+      );
+    } catch (error) {
+      return Utility.handleError(
+        res,
+        (error as TypeError).message,
+        ResponseCode.SERVER_ERROR
+      );
     }
+  }
 
-    async forgotPassword(req : Request, res : Response) {
-        try {
-            res.send({message: "Forgot Password mail sent"})
-        } catch (error) {
-            res.send({message: "Server Error"})
-        }
+  async forgotPassword(req: Request, res: Response) {
+    try {
+        // First thing, Get our params
+        const params = {...req.body}
+      res.send({ message: "Forgot Password mail sent" });
+    } catch (error) {
+      res.send({ message: "Server Error" });
     }
+  }
 
-    async resetPassword(req : Request, res : Response) {
-        try {
-            res.send({message: "Reset Password successful"})
-        } catch (error) {
-            res.send({message: "Server Error"})
-        }
+  async resetPassword(req: Request, res: Response) {
+    try {
+      res.send({ message: "Reset Password successful" });
+    } catch (error) {
+      res.send({ message: "Server Error" });
     }
+  }
 }
 
 export default UserController;
