c7ab5deb7cceb2c7d193434c58081358b487c210
John <johnadibe123@gmail.com>
2024-04-16 16:38:27 +0100

    Complete forgot password authentication feature

--- src/database/init.ts
+++ src/database/init.ts
@@ -1,11 +1,13 @@
 import Db from "./index"
 import userModel from "../models/user-model"
+import TokenModel from "../models/token-model"
 
 const DbInitialize = async () => {
     try {
         // if you want to connect to the database
         await Db.authenticate()
         userModel.sync({alter: false})
+        TokenModel.sync({alter: false})
     } catch(error) {
         console.log("Unable to connect to our database", error)
     }
