30bba943158058af1cfe319543df538e3cbdb5d5
John <johnadibe123@gmail.com>
2024-04-16 16:38:17 +0100

    Complete forgot password authentication feature

--- src/utils/index.utils.ts
+++ src/utils/index.utils.ts
@@ -12,11 +12,19 @@ const handleSuccess = (res:Response, message: string, data = {}, statusCode:numb
     return res.status(statusCode).json({status:true, message, data:{...data}})
 }
 
+const generateCode = (num: number = 15) => {
+    const dateString = Date.now().toString(36);
+    const randomness = Math.random().toString(36).substring(2);
+    let result = randomness + dateString;
+    result = result.length > num ? result.substring(0, num) : result;
+    return result.toUpperCase();
+}
 
 const Utility = {
     printRed, 
     handleError, 
-    handleSuccess
+    handleSuccess,
+    generateCode
 }
 
 export default Utility;
